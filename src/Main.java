d6629a9915177a1706cca6f250a6e27ffc48307c
unknown <241069>
2025-05-09 11:51:21 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("中原 唯緒");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
