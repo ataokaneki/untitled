57d7decbef616f9e9db76fccfb5483ec64982d81
ataokaneki <241069@std.hi-joho.ac.jp>
2025-05-23 11:39:25 +0900

    28,29

---
+++ src/reviews/reviews3/KeyInput.java
@@ -0,0 +1,17 @@
+package reviews.reviews3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class KeyInput {
+    public String readString(){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputStr = "";
+        try{
+            inputStr = br.readLine();
+        }catch(Exception e){
+            System.out.println("ERROR：IOException");
+        }
+        return inputStr;
+    }
+}
