768825a71b8241b9ab92b00574257a1df7208ee3
ataokaneki <241069@std.hi-joho.ac.jp>
2025-06-02 12:16:12 +0900

    授業 2025.6.2

---
+++ src/textbook_practice/chapter6/comment/kouhan.java
@@ -0,0 +1,13 @@
+package textbook_practice.chapter6.comment;
+
+public class kouhan {
+    public static void callDeae(){
+        System.out.println("ええい、こしゃくな。くせ者だ！であえい！");
+    }
+    public static void showMondokoro()throws Exception{
+        System.out.println("飛車さん、角さん。もういいでしょう。");
+        System.out.println("この紋所が目に入らぬか！");
+        java.lang.Thread.sleep(3000);  //練習問題6-3
+        zenhan.doTogame();
+    }
+}
---
+++ src/textbook_practice/chapter6/practice6_1.java
@@ -0,0 +1,12 @@
+package textbook_practice.chapter6;
+
+import textbook_practice.chapter6.comment.*;
+
+public class practice6_1 {
+    public static void main(String[] args)throws Exception {
+        zenhan.doWarusa();
+        zenhan.doTogame();
+        kouhan.callDeae();
+        kouhan.showMondokoro();
+    }
+}
